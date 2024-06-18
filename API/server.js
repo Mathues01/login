514aea79e0eee306eb601f8aac0c91125ba0f134
Matheus Romano Bonfim <157503343+Mathues01@users.noreply.github.com>
2024-06-17 21:59:17 -0300

    cadastro

---
+++ API/server.js
@@ -0,0 +1,68 @@
+import express from 'express'
+import cors from 'cors'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const users = []
+
+app.post('/usuarios', async (req, res) => {
+
+     await prisma.user.create({
+          data: {
+               email: req.body.email,
+               name: req.body.nome,
+               surname: req.body.sobrenome,
+               age: req.body.idade,
+               address: req.body.endereço
+          }
+     })
+
+     res.status(201).json(req.body)
+
+})
+
+app.get('/usuarios', async (req, res) => {
+
+     const users = await prisma.user.findMany()
+
+     res.status(200).json(users)
+})
+
+
+app.put('/usuarios/:id', async (req, res) => {
+     console.log(req)
+
+     await prisma.user.update({
+          where: {
+               id: req.params.id
+          },
+          data: {
+               email: req.body.email,
+               name: req.body.nome,
+               surname: req.body.sobrenome,
+               age: req.body.idade,
+               address: req.body.endereço
+          }
+     })
+
+     res.status(201).json(req.body)
+
+})
+app.delete('/usuarios/:id', async (req, res) => {
+
+     await prisma.user.delete({
+          where: {
+               id: req.params.id
+          }
+     })
+     res.status(200).json({ messeage: 'Usuário foi de Vasco' })
+})
+
+
+
+app.listen(3000)
---
+++ INTERFACE/Cadastro-do-Junin/src/pages/Home/index.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState, useRef } from 'react'
+import './style.css'
+import Trash from '../../assets/16qg.svg'
+import api from '../../services/api'
+
+function Home() {
+  const [users, setUsers] = useState([])
+
+  const inputName = useRef()
+  const inputSurname = useRef()
+  const inputEmail = useRef()
+  const inputAge = useRef()
+  const inputAddress = useRef()
+
+
+
+  async function getUsers() {
+    const usersFromApi = await api.get('/usuarios')
+
+    setUsers(usersFromApi.data)
+  }
+
+  async function createUsers() {
+    await api.post('/usuarios', {
+       nome: inputName.current.value,
+      sobrenome: inputSurname.current.value,
+      email: inputEmail.current.value,
+      idade: inputAge.current.value,
+      endereço: inputAddress.current.value
+
+    })
+    getUsers()
+ 
+   
+
+  } 
+  
+  async function deleteUsers(id) {
+     await api.delete(`/usuarios/${id}`)
+
+     getUsers()
+    }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  return (
+
+    <div className='container'>
+      <form action="">
+        <h1>Cadrasto</h1>
+        <input placeholder='Nome' name='nome' type='text' ref={inputName} />
+        <input placeholder='Sobreno' name='sobrenome' type='text' ref={inputSurname} />
+        <input placeholder='Email' name='email' type='email' ref={inputEmail} />
+        <input placeholder='Idade' name='idade' type='number' ref={inputAge} />
+        <input placeholder='Cidade' name='cidade' type='text' ref={inputAddress} />
+        <button type='button' onClick={createUsers}>Contratado</button>
+      </form>
+
+      {users.map((user) => (
+
+        <div key={user.id} className='card'>
+          <div>
+            <p>Nome: <span>{user.name} </span> </p>
+            <p>Sorenome: <span> {user.surname} </span> </p>
+            <p>Email: <span> {user.email} </span> </p>
+            <p>Idade: <span> {user.age} </span> </p>
+            <p>Cidade: <span>{user.address} </span> </p>
+          </div>
+          <button onClick={() => deleteUsers(user.id)}>
+            <img src={Trash} alt="Lixo" />
+          </button>
+        </div>
+      ))}
+
+    </div>
+
+  )
+}
+
+
+
+function Analisardados() {
+
+}
+
+export default Home
